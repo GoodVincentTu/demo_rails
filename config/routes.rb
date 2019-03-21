122f68bc3b3592fdc80490b4975bcd032475a29b
metatrontu@gmail.com <metatrontu@gmail.com>
2019-03-22 00:03:20 +0800

    Add controller testing gem and set up roues for registration and login process

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+  resources :users, only: [:new, :create, :edit, :update]
+  resources :sessions, only: [:new, :create, :destroy]
 end
